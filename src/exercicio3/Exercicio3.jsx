b22f25b262577d7ed131dcd6ee8f4f3e1cfa8750
Rafael Carvalho de Oliveira <rafael.carvalho.oliveira@gmail.com>
2020-09-10 22:26:19 -0300

    Exercicio 3, parte 2

--- src/exercicio3/Exercicio3.jsx
+++ src/exercicio3/Exercicio3.jsx
@@ -6,6 +6,8 @@ class Exercicio3 extends Component {
   constructor(props){
     super(props);
 
+    this.meuArg = 2;
+
     this.state = {
       meuTexto: ""
     }
@@ -13,12 +15,38 @@ class Exercicio3 extends Component {
 
   componentDidMount() {
     this.minhaFuncaoAssincrona1();
-    this.minhaFuncaoSincrona2();
+    //this.minhaFuncaoSincrona2();
   }
 
   minhaFuncaoAssincrona1 = async () => {
-    await this.sleep(5000);
-    console.log("oi, eu sou a função assíncrona 1!")
+
+    this.getEndereco("13272-315").then((endereco) => {
+      this.getMoradores(endereco).then((listaMoradores) => {
+        this.getIdadeFromMoradores(listaMoradores).then((idadeDosMoradores) => {
+          //codigo
+        }).catch((erro) => {
+
+        })
+      }).catch((erro) => {
+
+      })
+    }).catch((erro)=> {
+      console.log("ih, deu errado, o cep deve estar inválido. Erro retornado pelo servidor: " + erro);
+    })
+
+    try{
+      const endereco = this.getEndereco("13272-316");
+      //console.log("getEndereco passou sem erros!");
+      const moradores = await this.getMoradores(endereco);
+      //console.log("getMoradores passou sem erros!")
+      const idadeDosMoradores = await this.getIdadeFromMoradores(moradores);
+      //console.log("getIdadeFromMoradores passou sem erros!")
+
+      console.log("Todas as chamadas foram feitas com sucesso!")
+    } catch (meuErro) {
+      console.log("ih, deu errado, o cep deve estar inválido. Erro retornado pelo servidor: " + meuErro);
+    }
+    
   }
 
   minhaFuncaoSincrona2 = () => {
@@ -43,9 +71,14 @@ class Exercicio3 extends Component {
   }
 
 
-
-  sleep = (milliseconds) => {
-    return new Promise(resolve => setTimeout(resolve, milliseconds))
+  getEndereco = (cep) => {
+    return new Promise((resolve, reject) => {
+      if(cep === "13272-315") {
+        resolve("Rua João Previtale");
+        return;
+      }
+      reject("I don't know this CEP!");
+    });
   }
 }
 
